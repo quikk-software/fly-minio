8ec01b15c623535a92aeb4e60c496e372a798589
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2024-11-29 19:16:31 +0100

    🔧 Update

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
+
 cat <<EOF >> /etc/hosts
-fdaa:b:71d5:a7b:60:712e:59d6:2 minio1.local
-fdaa:b:71d5:a7b:73:25d6:ca0b:2 minio2.local
-fdaa:b:71d5:a7b:4e:791d:e60b:2 minio3.local
+fdaa:b:71d5:a7b:60:dc7b:3016:2  minio1.local
+fdaa:b:71d5:a7b:4e:1335:857c:2 minio2.local
+fdaa:b:71d5:a7b:73:fa1c:254c:2 minio3.local
 EOF
 
 # This script comes with the minio/minio image
